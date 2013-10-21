6b58698f545fff0892240a1171de0dcc963fe4b5
Robert Coleman <github@robert.net.nz>
2013-10-21 16:02:33 +1300

    loosen attribute default setting

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
-set['confd']['version'] = "0.1.1-beta"
-set['confd']['sha256'] = "07d569fcd77a0693879842a95b8f2840b82384a5b07281ae111562bd6bcc674e"
+default['confd']['version'] = "0.1.1-beta3"
+default['confd']['sha256'] = "7320b000c9528b03b260a08875d685b42692cad22e5e913132a4e67c44c324b8"
 
-set['confd']['confdir'] = "/etc/confd"
-set['confd']['interval'] = "600"
-set['confd']['prefix'] = "/"
-set['confd']['etcd_nodes'] = nil
-set['confd']['client_cert'] = nil
-set['confd']['client_key'] = nil
+default['confd']['confdir'] = "/etc/confd"
+default['confd']['interval'] = "600"
+default['confd']['prefix'] = "/"
+default['confd']['etcd_nodes'] = nil
+default['confd']['client_cert'] = nil
+default['confd']['client_key'] = nil
 
-set['confd']['extra'] = nil
+default['confd']['extra'] = nil
